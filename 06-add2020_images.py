263100090fc02b8d8d571b1cd0672ac7bb461d7b
Martin Schorb <schorb@embl.de>
2023-11-13 10:28:39 +0100

    first 2 2020 datasets

--- 06-add2020_images.py
+++ 06-add2020_images.py
@@ -3,11 +3,11 @@ import os
 import glob
 import multiprocessing
 
-datasets = ['VSM20_A1_AM1', 'VSM20_A2_PM1', 'VSM20_A3_AM2', 'VSM20_A4_PM2', 'VSM20_A5_PM3', 'VSM20_A6_AM3']
+datasets = ['VSM20_A1_AM1', 'VSM20_A2_PM1', 'VSM20_A3_AM2', 'VSM20_A4_PM2', 'VSM20_A5_AM3', 'VSM20_A6_PM3']
 source_dirs = ['tifA1-vsm20_bc', 'tifA2-vsm20_bc', 'tifA3-vsm20_bc', 'tifA4-vsm20bc', 'tifA5-vsm20_bc', 'tifA6-vsm20_bc']
 
 
-dataset, source_dir = list(zip(datasets, source_dirs))[5]
+dataset, source_dir = list(zip(datasets, source_dirs))[2]
 
 def add_tif(imfile):
     ds = mobie.metadata.read_dataset_metadata('./data/' + dataset)
@@ -25,12 +25,12 @@ def add_tif(imfile):
                         [1.676] * 2, [[2, 2]] * 6, [512, 512],
                         file_format="ome.zarr", menu_name="EM",
                         tmp_folder=None, target="slurm",
-                        max_jobs=5,
+                        max_jobs=6,
                         view=None, transformation=None,
                         unit="nm", )
 
 def add_images():
-    pool = multiprocessing.Pool(10)
+    pool = multiprocessing.Pool(25)
 
     imfiles = glob.glob(source_dir + '/*.tif')
 
