91768bdcffca75cb1e7ebd8020e80825213075cb
Martin Schorb <schorb@embl.de>
2023-11-14 12:05:20 +0100

    add all 2020 data

--- 06-add2020_images.py
+++ 06-add2020_images.py
@@ -7,7 +7,7 @@ datasets = ['VSM20_A1_AM1', 'VSM20_A2_PM1', 'VSM20_A3_AM2', 'VSM20_A4_PM2', 'VSM
 source_dirs = ['tifA1-vsm20_bc', 'tifA2-vsm20_bc', 'tifA3-vsm20_bc', 'tifA4-vsm20_bc', 'tifA5-vsm20_bc', 'tifA6-vsm20_bc']
 
 
-dataset, source_dir = list(zip(datasets, source_dirs))[3]
+dataset, source_dir = list(zip(datasets, source_dirs))[5]
 
 def add_tif(imfile):
     ds = mobie.metadata.read_dataset_metadata('./data/' + dataset)
@@ -31,6 +31,7 @@ def add_tif(imfile):
 
 def add_images():
     pool = multiprocessing.Pool(25)
+    for 
 
     imfiles = glob.glob(source_dir + '/*.tif')
 
